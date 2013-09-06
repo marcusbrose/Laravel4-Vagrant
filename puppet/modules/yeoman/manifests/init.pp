451798625104c3b23d55217271cbcaf7831a8d33
Marcus Brose <contact@marcusbrose.com>
2013-09-06 16:40:21 +0200

    ensure the latest version of npm

--- puppet/modules/yeoman/manifests/init.pp
+++ puppet/modules/yeoman/manifests/init.pp
@@ -12,21 +12,30 @@ class yeoman {
   package { 'yo':
     ensure => present,
     provider => 'npm',
-    require => [Class["nodejs"]],
+    require => [Class["nodejs"], Exec['update npm']],
   }
   package { 'grunt-cli':
     ensure => present,
     provider => 'npm',
-    require => [Class["nodejs"]],
+    require => [Class["nodejs"], Exec['update npm']],
   }
   package { 'bower':
     ensure => present,
     provider => 'npm',
-    require => [Class["nodejs"]],
+    require => [Class["nodejs"], Exec['update npm']],
+  }
+  package { 'n':
+    ensure => present,
+    provider => 'npm',
   }
   exec {
     'checking yeoman':
     command => "curl -L get.yeoman.io | bash",
     require => [Class["nodejs"], Class['other']],
+  }
+  exec { 
+    'update npm':
+    command => "n latest",
+    require => Package["n"],
   }
 }
